e514938206edaacf6849b51411d15a70c78993c9
saumyadip <saumyadip.biswas@isteer.com>
2022-04-18 17:46:22 +0530

    #6 - routes added

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 const routes: Routes = [{
   path:'signin',
   component: LoginFormComponent,
 },
-// {
-//   path:'signup',
-//   component: Regis
-// }
+{
+  path:'signup',
+  component: RegistrationFormComponent
+}
 ];
 
 @NgModule({
